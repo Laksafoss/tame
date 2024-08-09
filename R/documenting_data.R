57684c306f203c994d8d051d7cd185c8c852c4be
Laksafoss <adls@ssi.dk>
2024-08-09 12:47:52 +0200

    chore: Write print method for `summary.medic` objects

--- R/documenting_data.R
+++ R/documenting_data.R
@@ -7,9 +7,9 @@
 "eczema"
 
 #' A Simulated Data Set About Pregnancy Complications
-#' 
-#' We use this data set in all the examples in the package.  
-#' 
+#'
+#' We use this data set in all the examples in the package.
+#'
 #' @name complications
 #' @docType data
 #' @keywords data
